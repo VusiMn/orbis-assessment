0454e86214a5e85baf073e4d1e43683cfdd03900
vusimn <vusi.mngomezulu@allangray.co.za>
2022-08-11 17:46:12 +0200

    game player simulation draft1

--- src/main/scala/GamePlayer.scala
+++ src/main/scala/GamePlayer.scala
@@ -6,21 +6,34 @@ object GamePlayer extends App {
   val secondPlayer = new Player("Dalisani")
   val random = Random// random generate score
 
-  var setWinner = false
+  var setWinnerStatus = false
 
   val setGames = new SetGames()
 
-  while(setGames.setWon(firstPlayer.gamesWon, secondPlayer.gamesWon) || setGames.setWon(secondPlayer.gamesWon, firstPlayer.gamesWon)) {
+  val matchGames = new Match()
+
+  while(setWinnerStatus == false) {
 
     firstPlayer.score = random.nextInt(100)
     secondPlayer.score = random.nextInt(100)
 
     if(firstPlayer.score > secondPlayer.score) {
       firstPlayer.gamesWon +=1
+      setWinnerStatus = setGames.setWon(firstPlayer.gamesWon, secondPlayer.gamesWon)
       println(s" ${firstPlayer.name } won a game score ${firstPlayer.score} - ${secondPlayer.score}")
+      if(setWinnerStatus){
+        println(s" ${firstPlayer.name } Won a SET games won score ${firstPlayer.gamesWon} - ${secondPlayer.gamesWon} " +
+          s" with games won greater then ${setGames.maximumGamesWon} and game margin greater then ${setGames.minimumGamesWonMargin}")
+      }
     } else{
       secondPlayer.gamesWon +=1
+      setWinnerStatus = setGames.setWon(secondPlayer.gamesWon, firstPlayer.gamesWon)
       println(s" ${secondPlayer.name } won a game score ${secondPlayer.score} - ${firstPlayer.score}")
+
+      if(setWinnerStatus){
+        println(s" ${secondPlayer.name } won SET games won score ${secondPlayer.gamesWon} - ${firstPlayer.gamesWon} " +
+          s" with games won greater then ${setGames.maximumGamesWon} and game margin greater then ${setGames.minimumGamesWonMargin}")
+      }
     }
 
 
